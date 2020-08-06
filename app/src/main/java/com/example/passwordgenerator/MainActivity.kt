4332a43c2764bdd0d7f3c73a726e4b76a357743b
Dru <andru.rosen@gmail.com>
2020-08-06 11:31:14 -0400

    Hide app contents in app switcher

--- app/src/main/java/com/example/passwordgenerator/MainActivity.kt
+++ app/src/main/java/com/example/passwordgenerator/MainActivity.kt
@@ -3,18 +3,17 @@ package com.example.passwordgenerator
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.os.Bundle
-import android.util.Log
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import org.w3c.dom.Node
-import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
     private val symbolSet : List<Char> = ('!'..'/') + (':'..'@') + ('['..'`') + ('{'..'~')
     private val charSet : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
     private val allChars = charSet + symbolSet
-    
+
     private var myClipboard: ClipboardManager? = null
     private var myClip: ClipData? = null
 
@@ -46,7 +45,6 @@ class MainActivity : AppCompatActivity() {
                     randCharSet += allChars.random().toString()
                 }
             }
-
             passwordLength.text.clear()
         } else {
             val toast = Toast.makeText(applicationContext, "Please enter a number", Toast.LENGTH_SHORT)
@@ -60,5 +58,18 @@ class MainActivity : AppCompatActivity() {
         myClipboard?.primaryClip = myClip
 
         Toast.makeText(this, "Text Copied", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
     }
 }
